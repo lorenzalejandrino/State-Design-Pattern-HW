0807af9b543f8bc1eeb86f455e87dc0eecb05a2c
lorenzalejandrino <158862246+lorenzalejandrino@users.noreply.github.com>
2025-02-22 13:17:57 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,32 @@ public class Main {
     public static void main(String[] args) {
         VendingMachine machine = new VendingMachine();
 
+        System.out.println("Selecting Item");
         machine.selectItem();
-        machine.insertCoin(5);
-        machine.insertCoin(5);
         System.out.println("");
+
+        System.out.println("Inserting Coins");
+        machine.insertCoin(30);
+        machine.insertCoin(30);
+        System.out.println("");
+
+        System.out.println("Selecting Item");
         machine.selectItem();
-        machine.insertCoin(10);
         System.out.println("");
+
+        System.out.println("Inserting Extra Coin");
+        machine.insertCoin(50);
+        System.out.println("");
+
+        System.out.println("Setting Machine Out of Order");
         machine.setOutOfOrder();
-        machine.insertCoin(5);
+        System.out.println("");
+
+        System.out.println("Trying to Select in OutOfOrderState");
+        machine.selectItem();
+        System.out.println("");
+
+        System.out.println("Trying to Insert Coin in OutOfOrderState");
+        machine.insertCoin(50);
     }
 }
